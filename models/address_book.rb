32c5eb31598fda69840bf9deb57079d78fbd02ef
Joseph Hakoopian <joseph.hakoopian@gmail.com>
2017-02-25 14:23:08 -0500

    as7: add second csv file and tests

--- models/address_book.rb
+++ models/address_book.rb
@@ -33,10 +33,8 @@ class AddressBook
 
 
   def import_from_csv(file_name)
-    # Implementation goes here
     csv_text = File.read(file_name)
     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
-    # #8
     csv.each do |row|
       row_hash = row.to_hash
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
