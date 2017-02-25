13b461217518b720ffa5e7c8df116bed3eb8dbcf
Joseph Hakoopian <joseph.hakoopian@gmail.com>
2017-02-25 14:07:41 -0500

    add CSV file and tests

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -1,4 +1,3 @@
-# #1
 require_relative '../models/address_book'
 
 class MenuController
@@ -9,7 +8,6 @@ class MenuController
   end
 
   def main_menu
-    # #2
     puts "Main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
     puts "2 - Create an entry"
@@ -19,10 +17,8 @@ class MenuController
     puts "6 - Exit"
     print "Enter your selection: "
 
-    # #3
     selection = gets.to_i
 
-    # #7
     case selection
       when 1
         system "clear"
@@ -46,9 +42,7 @@ class MenuController
         main_menu
       when 6
         puts "Good-bye!"
-        # #8
         exit(0)
-      # #9
       else
         system "clear"
         puts "Sorry, that is not a valid input"
@@ -56,13 +50,10 @@ class MenuController
     end
   end
 
-  # #10
   def view_all_entries
     address_book.entries.each do |entry|
     system "clear"
     puts entry.to_s
-  # #15
-    # entry_submenu(entry)
     entry_submenu
 end
 
@@ -71,10 +62,8 @@ puts "End of entries"
   end
 
   def create_entry
-    # #11
     system "clear"
     puts "New AddressBloc Entry"
-    # #12
     print "Name: "
     name = gets.chomp
     print "Phone number: "
@@ -82,7 +71,6 @@ puts "End of entries"
     print "Email: "
     email = gets.chomp
 
-    # #13
     address_book.add_entry(name, phone, email)
 
     system "clear"
@@ -95,24 +83,18 @@ puts "End of entries"
   def read_csv
   end
 
-  # def entry_submenu(entry)
   def entry_submenu
-    # #16
     puts "n - next entry"
     puts "d - delete entry"
     puts "e - edit this entry"
     puts "m - return to main menu"
 
-    # #17
     selection = gets.chomp
 
     case selection
-    # #18
       when "n"
-    # #19
       when "d"
       when "e"
-    # #20
       when "m"
         system "clear"
         main_menu
@@ -129,8 +111,6 @@ puts "End of entries"
     puts "Input the entry number you would like to search for"
     entry_number = gets.to_i - 1
 
-    # left in this while statement to remember a solution that was problematic
-    # while entry_number + 1 > address_book.entries.count
     if entry_number + 1 > address_book.entries.count
       system "clear"
       puts "Invalid input. Press enter for main menu"
--- models/address_book.rb
+++ models/address_book.rb
@@ -1,32 +1,45 @@
 require_relative 'entry'
+require "csv"
 
-  class AddressBook
-    attr_reader :entries
+class AddressBook
+  attr_reader :entries
 
-    def initialize
-      @entries = []
-    end
+  def initialize
+    @entries = []
+  end
 
-    def add_entry(name, phone_number, email)
-      index = 0
-      entries.each do |entry|
-        if name < entry.name
-          break
-        end
-        index+= 1
+  def add_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+      if name < entry.name
+        break
       end
-      entries.insert(index, Entry.new(name, phone_number, email))
+      index += 1
     end
+    entries.insert(index, Entry.new(name, phone_number, email))
+  end
 
-    def remove_entry(name, phone_number, email)
-      index = 0
-      entries.each do |entry|
-        if name == book.name && phone_number == book.phone_number && email == book.email
-          break
-        end
-        index += 1
+  def remove_entry(name, phone_number, email)
+    index = 0
+    entries.each do |entry|
+      if name == entry.name && phone_number == entry.phone_number && email == entry.email
+        break
       end
-      entries.delete(index)
-      return "Deleted #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
+      index += 1
+    end
+    entries.delete_at(index)
+    entries
+  end
+
+
+  def import_from_csv(file_name)
+    # Implementation goes here
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    # #8
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
   end
+end
